855fbaedbbd85e88e43778b32bd227dbd38008ff
beatriz <biia.perez10@gmail.com>
2020-12-15 18:40:55 -0300

    Ajustes

--- script.js
+++ script.js
@@ -7,37 +7,46 @@ var sec_listaContato = document.getElementById("sec-listaContato");
 
 var ListaNome = [];
 
-hamburguer.addEventListener("click",function(){
+hamburguer.addEventListener("click", function () {
     var barralat = document.querySelector(".barralat").classList.toggle("show-menu");
 });
 
-btn_adicionar.addEventListener("click", function(evento){
-    evento.defaultPrevented();
+btn_adicionar.addEventListener("click", function (evento) {
+    evento.preventDefault();
     montaLista();
 })
-input_nome.addEventListener("focus", function(evento){
+input_nome.addEventListener("focus", function (evento) {
     evento.target.select();
-}) 
+})
 
-function montaLista(){
+function montaLista() {
     var nomeInputado = input_nome.value;
     var emailInputado = input_email.value;
     var avatarInputado = input_avatar.value;
 
     var NovaTag = ''
 
-    ListaNome.push({nome: nomeInputado, email: emailInputado, avatar: avatarInputado});
-    ListaNome.map(function(obj){
-        NovaTag += `li class="card">
+    ListaNome.push({ nome: nomeInputado, email: emailInputado, avatar: avatarInputado });
+    ListaNome.map(function (obj) {
+        NovaTag += `
+        <li class="card">
         <button class="card-edit-butt">  
              <i class="far fa-edit"></i>
         </button>
-        <img src="https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/80.png" alt="" class="card-midia">
-        <h3>${obj.input_nome}</h3>
+        <img src="${obj.avatar}" alt="" class="card-midia">
+        <h3>${obj.nome}</h3>
+        <h3>${obj.email}</h3>
 
     </li>`
     })
 
     sec_listaContato.innerHTML = NovaTag;
 
+    limpaTela();
+}
+
+function limpaTela() {
+    input_nome.value = "";
+    input_email.value = "";
+    input_avatar.value = "";
 }
